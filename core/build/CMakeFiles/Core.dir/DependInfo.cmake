f2b07f1c4488507cdc52302c7ec1ef8d62a2e012
binaryHips <tom.zinck@etu.umontpellier.fr>
2025-11-04 16:31:21 +0100

    first commit

---
+++ core/build/CMakeFiles/Core.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/e20210002460/Master/M2/Projet3D/core/commons/image/image.cpp" "CMakeFiles/Core.dir/commons/image/image.cpp.o" "gcc" "CMakeFiles/Core.dir/commons/image/image.cpp.o.d"
+  "/home/e20210002460/Master/M2/Projet3D/core/commons/particleSystem/particleSystem.cpp" "CMakeFiles/Core.dir/commons/particleSystem/particleSystem.cpp.o" "gcc" "CMakeFiles/Core.dir/commons/particleSystem/particleSystem.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ core/defines.h
@@ -0,0 +1,70 @@
+#pragma once
+
+// Definitions
+
+#ifndef NDEBUG
+#define DEBUG false
+#else
+#define DEBUG true
+#endif
+
+#define DEBUG_EXCEPT = \
+#if DEBUG
+#else noexcept
+#endif
+
+// assertions (https://www.foonathan.net/2016/09/assertions/)
+namespace {
+    #include <stdio>
+
+    struct source_location{
+        const char* file_name;
+        unsigned line_number;
+        const char* function_name;
+    };
+
+
+    class BaseHandler{
+        static void handle(const source_location& loc, const char* expr) noexcept {
+            std::cout << "ASSERTION FAILED\n" <<
+            "   File " loc.file_name "\n"
+            "   Function " loc.function_name "\n"
+            "   Line " std::to_string(line_number) "\n"
+            "On expression ( " expr " )" 
+            << std::endl;
+        }
+    };
+
+    template <class Handler, class Expr>
+    void do_assert(std::true_type, const Expr& e, const source_location& loc, const char* expression) noexcept
+    {
+        if (!e())
+        {
+            Handler::handle(loc, expression);
+            std::abort();
+        }
+    }
+
+    template <class Handler, class Expr>
+    void do_assert(std::false_type, const Expr&, const source_location&, const char*) noexcept {}
+
+    template <class Handler, class Expr>
+    void do_assert(const Expr& e, const source_location& loc, const char* expression){
+        do_assert<Handler>(Handler{}, e, loc, expression);
+    }
+
+    #define ASSERT(Expr, Handler) \
+        do_assert<Handler>([&] { return Expr; }, CUR_SOURCE_LOCATION, #Expr)
+}
+
+
+
+// Type aliases
+
+using u32 = unsigned int;
+using u16 = unsigned short;
+using u8 = unsigned char;
+
+using ParticleId = u32;
+using Pixel = u8;
+
