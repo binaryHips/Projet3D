fc910431cf5fce16d7a7b464b5eaa78bf38eec6a
binaryHips <tom.zinck@etu.umontpellier.fr>
2025-11-28 14:53:12 +0100

    removed a print

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ enum class MAP_LAYERS {
 int main(int, char**){
     GeoContextCPU context = GeoContextCPU::createGeoContext();
     context.update(0.1);
+    context.update(0.1);
+    context.update(0.1);
     //Pixel &currentPixel = context.maps[to_underlying(MAP_LAYERS::SAND)](10, 10);
     //std::cout << currentPixel << std::endl;
     std::cout << context.totalHeight(uvec2(10, 10)) << std::endl;
