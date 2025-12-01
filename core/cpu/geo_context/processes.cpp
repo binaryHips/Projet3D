f4486d9d36b6ec782b3e33ec5853a140be6bb592
binaryHips <tom.zinck@etu.umontpellier.fr>
2025-12-01 17:10:55 +0100

    sand??????

--- core/cpu/geo_context/processes.cpp
+++ core/cpu/geo_context/processes.cpp
@@ -4,12 +4,31 @@
 
 void processSandGravity(GeoContextCPU &context, float delta){
 
-    for (u32 i = 0; i < IMGSIZE; ++i) for (u32 j = 0; j < IMGSIZE; ++j){
-        Pixel &currentPixel = context.maps[to_underlying(MAP_LAYERS::SAND)](i, j);
-        float currentHeight = context.totalHeight(uvec2(i, j));
-        
-        float new_val; // TODO use kernel to get total height then faaaaall
+    for (u32 i = 0; i < IMGSIZE-1; ++i) for (u32 j = 0; j < IMGSIZE-1; ++j){
+        u32 sandLayerIndex = to_underlying(MAP_LAYERS::SAND);
 
+        Pixel &currentPixel = context.maps[sandLayerIndex](i, j);
+        float currentHeight = context.heightTo(uvec2(i+1, j), sandLayerIndex);
+
+
+        const float max_slope = 0.4;
+        const float maxDisplaceQuantity = 1.0; // x times the difference in height.
+        
+        // adapted from "Realtime Procedural Terrain Generation. Realtime Synthesis of Eroded Fractal Terrain for Use in Computer Games"
+        float grad_x = context.heightTo(uvec2(i+1, j), sandLayerIndex) - currentHeight;
+        float grad_y = context.heightTo(uvec2(i, j+1), sandLayerIndex) - currentHeight;
+
+        if (abs(grad_x) > max_slope | abs(grad_y) > max_slope){
+            if (abs(grad_x) > abs(grad_y)){
+                float displaceToCurrent = maxDisplaceQuantity * grad_x * delta;
+                currentPixel += displaceToCurrent;
+                context.maps[sandLayerIndex](i+1, j) -= displaceToCurrent;
+            } else {
+                float displaceToCurrent = maxDisplaceQuantity * grad_y * delta;
+                currentPixel += displaceToCurrent;
+                context.maps[sandLayerIndex](i, j+1) -= displaceToCurrent;
+            }
+        } 
     }
 }
 
