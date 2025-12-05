07a80d1bf545453118f62a7be913b4f914b412f3
binaryhips <binarheap@gmail.com>
2025-12-05 14:31:33 +0100

    fixed thermal erosion

--- core/cpu/geo_context/processes.cpp
+++ core/cpu/geo_context/processes.cpp
@@ -10,8 +10,8 @@ void thermalErode(GeoContextCPU &context, float delta){
         Pixel &currentPixel = context.maps[layerIndex](i, j);
         double currentHeight = context.heightTo(uvec2(i, j), layerIndex);
 
-        const float max_slope = 0.1 * (1.0 / IMGSIZE);
-        const double maxDisplaceQuantity = 0.5 * delta; // x times the difference in height.
+        const float maxSlope = 0.1 * (1.0 / IMGSIZE);
+        const double maxDisplaceQuantity = 0.5; // x times the difference in height.
         
         // adapted from "Realtime Procedural Terrain Generation. Realtime Synthesis of Eroded Fractal Terrain for Use in Computer Games"
 
@@ -48,10 +48,10 @@ void thermalErode(GeoContextCPU &context, float delta){
             maxgradDir = maxgradDir_;
         }
 
-        if (maxGrad > 0.0){
-            float displaceToCurrent = std::clamp(maxGrad * delta, -maxDisplaceQuantity, maxDisplaceQuantity);
-            currentPixel += displaceToCurrent;
-            context.maps[layerIndex](maxgradDir[0], maxgradDir[1]) -= displaceToCurrent;
+        if (maxGrad > maxSlope){
+            float displaceToCurrent = std::min(maxGrad * delta, maxGrad / 2.0);
+            currentPixel -= displaceToCurrent;
+            context.maps[layerIndex](maxgradDir[0], maxgradDir[1]) += displaceToCurrent;
         } 
     }
 }
