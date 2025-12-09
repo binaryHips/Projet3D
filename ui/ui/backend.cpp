68258ddff28f531f584485758fec5f3968bbc5f7
Andrew Mansour <andrew.mansour24@gmail.com>
2025-12-09 15:48:28 +0100

    a tiny bit prettier particles

--- ui/ui/backend.cpp
+++ ui/ui/backend.cpp
@@ -125,7 +125,7 @@ void Backend::drawParticles(QOpenGLExtraFunctions *gl_funcs, const ParticleSyste
 
     particleShader->bind();
     particleShader->setUniformValue("MVP", MVP);
-    particleShader->setUniformValue("pointSize", 100.0f);  
+    particleShader->setUniformValue("pointSize", 10.0f);  
     particleShader->setUniformValue("particleColor", QVector4D(1.0f, 0.0f, 0.0f, 1.0f));  
 
     gl_funcs->glBindVertexArray(particleVAO);
